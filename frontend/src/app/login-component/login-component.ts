01bcce120f303a549e0f5e790c3c5cdb3a30ade4
waleedrimawi1 <waleedrimawi1@gmail.com>
2025-09-07 02:54:11 +0300

    build entities, repositories and security configuration

---
+++ frontend/src/app/login-component/login-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  standalone: true,
+  selector: 'app-login-component',
+  imports: [],
+  templateUrl: './login-component.html',
+  styleUrl: './login-component.css'
+})
+export class LoginComponent {
+
+}
