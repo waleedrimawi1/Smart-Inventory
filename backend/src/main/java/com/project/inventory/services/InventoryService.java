ce9fa6536b3e7424fe3e1e151d98e8137d5ec767
ShahdYahya21 <shahd.yahya2003@gmail.com>
2025-09-14 00:14:33 +0300

    resolve conflicts

--- backend/src/main/java/com/project/inventory/services/InventoryService.java
+++ backend/src/main/java/com/project/inventory/services/InventoryService.java
@@ -1,12 +1,27 @@
 package com.project.inventory.services;
 
+import com.project.inventory.entity.RoleEnum;
+import com.project.inventory.entity.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 public interface InventoryService {
 
     public Map<String, Object> login(Map<String, String> request);
+    public boolean isEmailTaken(String email);
+    public List<User> getAllUsers();
+    public Optional<User> getUserById(Integer id);
+    public List<User> getUsersByRole(RoleEnum roleName);
+    public User createUser(String fullName, String email, String password, String phone, RoleEnum roleEnum);
+    public User updateUserRole(Integer userId, RoleEnum newRole);
+    public void deleteUser(Integer userId);
 
-}
+
+
+    }
